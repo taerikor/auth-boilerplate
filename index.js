bf65cf5168323746c6931f612f75b9aa2eac9442
taerikor <akdud153@gmail.com>
2021-01-18 03:05:42 +0900

    connected MongoDB

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://taeri:asd153zz@boilerplate-shard-00-00.xglo6.mongodb.net:27017,boilerplate-shard-00-01.xglo6.mongodb.net:27017,boilerplate-shard-00-02.xglo6.mongodb.net:27017/boilerplate?ssl=true&replicaSet=atlas-2dxgt2-shard-0&authSource=admin&retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB Connected'))
   .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안뇽')
+  res.send('Hello World! 안녕하세요!')
 })
+
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
