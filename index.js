7ab66994ba32bbe9fb377f1c62bfec711e715708
taerikor <akdud153@gmail.com>
2021-10-30 00:46:02 +0900

    Add user schema, nodemon

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const config = require("./config/key");
+const { User } = require("./models/User");
 
-mongoose.connect(
-  "mongodb+srv://<username>:<password>@boilerplate.xglo6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {}
-);
+mongoose
+  .connect(config.mongoURL)
+  .then(() => console.log("Server Connected!"))
+  .catch((err) => console.log(err));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!");
+});
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
