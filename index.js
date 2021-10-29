abac86ee68f4892df8aa2d59e56ed0455a67c5af
taerikor <akdud153@gmail.com>
2021-10-29 23:14:55 +0900

    Add mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://<username>:<password>@boilerplate.xglo6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {}
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
